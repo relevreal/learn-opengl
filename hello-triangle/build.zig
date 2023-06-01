215378ae297cace032d8d6a83106eb6d4dd851c6
relereal <mindfigment@protonmail.com>
2023-06-01 14:17:03 +0200

    feat: add hello-triangle

---
+++ hello-triangle/build.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+const currentTarget = @import("builtin").target;
+const Builder = std.build.Builder;
+
+pub fn build(b: *Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "introduction-exe",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // includes
+    exe.addIncludePath("/usr/local/include");
+    exe.addIncludePath("deps/include");
+
+    // sources
+    exe.addCSourceFile("deps/src/glad.c", &[_][]const u8{"-std=c99"});
+
+    switch (currentTarget.os.tag) {
+        .linux => {
+            exe.addLibraryPath("/usr/lib/x86_64-linux-gnu");
+            exe.linkSystemLibrary("c");
+            exe.linkSystemLibrary("gl");
+        },
+        else => {
+            @panic("don't know how to build on your system");
+        },
+    }
+    exe.linkSystemLibrary("glfw3");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+    // const run_step = b.step("run", "Run the app");
+    // run_step.step.dependOn(&run_cmd.step);
+    // const compile_step = b.step("compile", "Compiles src/main.zig");
+    // compile_step.dependOn(&exe.step);
+
+    // const install_step = b.addInstallArtifact(compile_step);
+    // install_step.step.dependOn(b.getInstallStep());
+
+    // const run_cmd = b.addRunArtifact(exe);
+    // run_cmd.step.dependOn(b.getInstallStep());
+
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
+}
